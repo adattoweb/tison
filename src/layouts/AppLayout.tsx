6b3344cbbf45b8f022378676b6249d09de4ccac3
adattoweb <adattostudio174@gmail.com>
2026-09-15 11:10:22 +0300

    installed and initialized tanstack-query

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
 import { Sidebar } from "@/components/Sidebar/Sidebar"
 import type { PropsWithChildren } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 export function AppShell({ children }: PropsWithChildren) {
    return (
@@ -14,9 +15,12 @@ export function AppShell({ children }: PropsWithChildren) {
 }
 
 export function AppLayout() {
+   const queryClient = new QueryClient()
    return (
-      <AppShell>
-         <Outlet />
-      </AppShell>
+      <QueryClientProvider client={queryClient}>
+         <AppShell>
+            <Outlet />
+         </AppShell>
+      </QueryClientProvider>
    )
 }
